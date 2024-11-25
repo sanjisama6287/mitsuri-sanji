f0212e69833133ce4184d4ebfe2ef7e73125b0a7
ɪɴᴠᴀʟɪᴅ <116873243+ifeelscam@users.noreply.github.com>
2024-11-25 12:03:19 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -24,5 +24,5 @@ def get_current_time():
 
 
 def shorten_url(url):
-    site_url = f"https://Publicearn.com/api?api=fd0f68b969f0b61e5f274f9a389d3df82faec11e&url={url}&format=text"
+    site_url = f"https://Publicearn.com/api?api=b67ced42d19196b20d07581beece1112607ba543&url={url}&format=text"
     return str(requests.get(site_url).text)
