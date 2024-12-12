ab4d22e43c1a9c8e6a73dd0e48f275b5254b7d5a
Sanji Sama <sanjisama6287@genvolvo.lol>
2024-12-13 03:24:04 +0530

    Add files via upload

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -6,21 +6,22 @@ from config import *
 from pyrogram import enums
 from pyrogram.types import Message, InputMediaPhoto, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-message_content = '''👋 Hey {first}\n
+message_content = '''👋 Hey {first} 🏆\n
 🎖️ Available Plans :\n
 ● 149 rs For 1 Month Prime Membership\n
 ● 399 rs For 3 Months Prime Membership\n
-● 699 rs For 6 Year Prime Membership\n
-💵 UPI ID - Contact <a href="https://t.me/Syfer_Admin_Bot">Syfer</a>\n
-📸 QR - <a href="https://envs.sh/UvX.jpg">ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ꜱᴄᴀɴ</a>\n
-♻️ <b>Please Contact Admin For Any Type Of Support You Want From Us !!</b>'''
+● 699 rs For 6 Year Prime Membership\n\n
+💵 UPI ID - <a href="https://t.me/Syfer_Admin_Bot">Contact Syfer</a>\n\n
+📸 QR - <a href="https://envs.sh/UvX.jpg">ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ꜱᴄᴀɴ</a>\n\n
+♻️ <b>Please Contact Admin For Any Type Of Support You Want From Us !!\n\n
+‼️ ᴍᴜsᴛ sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛ ᴀғᴛᴇʀ ᴘᴀʏᴍᴇɴᴛ</b>'''
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ 𝐎ᴡɴᴇʀ : <a href='https://t.me/Syfer_Admin_Bot'>Syfer</a></b>",
+            text = f"<b>○ 𝐎ᴡɴᴇʀ : <a href='https://t.me/Syfer_Admin_Bot'>Syfer</a></b>\n\nBaaki Kuch Nahi Batane wala 👽👍😂",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
@@ -33,7 +34,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     
     elif query.data == "plan":
         btn = [[
-            InlineKeyboardButton('📬    Contact OWNER to Buy Plan', url="https://t.me/Syfer_Admin_Bot")
+            InlineKeyboardButton('📬Contact OWNER to Buy a Plan 📬', url="https://t.me/Syfer_Admin_Bot")
         ],[
             InlineKeyboardButton(' ᴄʟᴏꜱᴇ ', callback_data='close')
         ]]
